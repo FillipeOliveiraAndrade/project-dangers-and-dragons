52d535faf54faf3cd5a3d6d491c0b6b006884fd8
fillipe <fillipe.trybe@gmail.com>
2023-03-13 19:02:53 -0300

    Requirement thirteen

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Castiel');
+const player2 = new Character('Dean');
+const player3 = new Character('Sam');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export { 
+  player1, 
+  player2, 
+  player3, 
+
+  monster1,
+  monster2,
+
+  pvp,
+  pve,
+
+  runBattles,
+};
